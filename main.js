725980d13f7fa90573d29175784619fcae99965f
Klaudia <oska456@wp.pl>
2020-10-22 19:46:42 +0200

    update style.css, menu

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const btn=document.querySelector('.hamburger');
-const brg=document.querySelector('.nav-desktop')
+const brg=document.querySelector('.nav-desktop');
+const ul=document.querySelector('.nav-ul');
 
 
 btn.addEventListener('click', () => {
-    brg.classList.toggle('show')
+    brg.classList.toggle('show');
+    
 });
+
+
+
